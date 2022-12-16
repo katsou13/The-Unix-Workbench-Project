0bf603cb672594b6106157c97c5c907e73aa39fd
katsou13 <117817086+katsou13@users.noreply.github.com>
2022-12-16 12:53:54 +0200

    Create guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,25 @@
+ilenumber=$(ls -f | wc -l)
+function response {
+        read response
+}
+echo "How many files the current directory has?"
+response
+echo "You entered:$response"
+while [[ $response -ne $filenumber ]] 
+do
+if [[ $response -gt $filenumber ]]
+then
+        echo "You're answer is too high"
+        echo "Please try again and press enter: "
+        response
+else  
+        echo "You're answer is too low"
+        echo "Please try again and press enter: "
+        response
+fi
+done
+if [[ $response = $filenumber ]]
+then
+        echo "Congratulations, you're answer is correct!"    
+fi
+
